533acc15eec51aaa353dc60e0ebf6188a4e5aba8
Tomas Perez Segura <tperezsegura1@gmail.com>
2021-04-15 23:49:40 -0300

    Ejercicio corregido

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include <fstream>
-using namespace std;
 
 int main() {
-    ofstream file("output.txt");
-    file << "Hello, World!";
-    file.close();
-
-    return 0;
+    std::cout << "Hello, World!";
 }
